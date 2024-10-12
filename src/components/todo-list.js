d6ac8ce8797e46928f1a431b7ed462b6915306a5
Chloe <chloe@sapphic.moe>
2024-10-12 12:01:09 +0500

    step 7: using arrays

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,20 +1,20 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <ul>
       <li>
-        <TodoListItem label="Drink Coffee" />
+        <TodoListItem label={todos[0].label} important={todos[0].important} />
       </li>
       <li>
-        <TodoListItem label="Implement React Application" />
+        <TodoListItem label={todos[1].label} important={todos[1].important} />
       </li>
       <li>
-        <TodoListItem label="Make notes from your study" />
+        <TodoListItem label={todos[2].label} important={todos[2].important} />
       </li>
       <li>
-        <TodoListItem label="Do not drink alchohol" important />
+        <TodoListItem label={todos[3].label} important={todos[3].important} />
       </li>
     </ul>
   );
