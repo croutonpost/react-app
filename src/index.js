6346b71d67c3face31624806e4313a1324e0675b
Chloe <chloe@sapphic.moe>
2024-10-12 12:25:08 +0500

    step 12: refactor structure

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppHeader from "./components/header";
-import SearchPanel from "./components/search";
+
+import Header from "./components/header";
+import Search from "./components/search";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
+
+import "./index.css";
 
 const App = () => {
   const todoData = [
     { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Implement React Application", important: true, id: 2 },
+    { label: "React Application", important: true, id: 2 },
     { label: "Make notes from your stud", important: false, id: 3 },
     { label: "Do not drink alchohol", important: false, id: 4 },
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <Header toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <Search />
+        <ItemStatusFilter />
+      </div>
+
       <TodoList todos={todoData} />
     </div>
   );
 };
+
 ReactDOM.render(<App />, document.getElementById("root"));
