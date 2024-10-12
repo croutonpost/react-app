8916bb95211158dde416b1c3719bea8dcd566ad4
Chloe <chloe@sapphic.moe>
2024-10-12 12:06:12 +0500

    step 9: key parameter

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,9 +3,10 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({ todos }) => {
   const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
     return (
-      <li>
-        <TodoListItem label={item.label} important={item.important} />
+      <li key={item.id}>
+        <TodoListItem {...itemProps} />
       </li>
     );
   });
