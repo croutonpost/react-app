72b1cb45267671400fb86de67bf49cd38797ee3c
Chloe <chloe@sapphic.moe>
2024-10-12 12:02:16 +0500

    step 8: map method

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,22 +2,15 @@ import React from "react";
 import TodoListItem from "./todo-list-item";
 
 const TodoList = ({ todos }) => {
-  return (
-    <ul>
+  const elements = todos.map((item) => {
+    return (
       <li>
-        <TodoListItem label={todos[0].label} important={todos[0].important} />
+        <TodoListItem label={item.label} important={item.important} />
       </li>
-      <li>
-        <TodoListItem label={todos[1].label} important={todos[1].important} />
-      </li>
-      <li>
-        <TodoListItem label={todos[2].label} important={todos[2].important} />
-      </li>
-      <li>
-        <TodoListItem label={todos[3].label} important={todos[3].important} />
-      </li>
-    </ul>
-  );
+    );
+  });
+
+  return <ul>{elements}</ul>;
 };
 
 export default TodoList;
