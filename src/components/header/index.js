12b07754d227a211d39f9efaa3bca58248458831
Chloe <chloe@sapphic.moe>
2024-10-19 12:40:07 +0500

    step 33: prettify header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 export default class AppHeader extends React.Component {
   render() {
     return (
-      <div className={"app-header d-flex"}>
+      <div className="app-header">
         <h1>Todo List</h1>
         <h2>
-          {this.props.toDo} more to do
+          {!this.props.toDo ? "All tasks are done!" : `${this.props.toDo} more to do, ${this.props.done} done`}
         </h2>
       </div>
     );
