bc012f7cbf15192d27bc1684e0a5432ea6effc32
Chloe <chloe@sapphic.moe>
2024-10-19 12:08:44 +0500

    step 20: use iconify and use delete logic

--- src/components/todo-list-item/index.js
+++ src/components/todo-list-item/index.js
@@ -1,24 +1,21 @@
 import React from "react";
 import "./index.css";
+import { Icon } from "@iconify/react";
+
 export default class TodoListItem extends React.Component {
   state = {
     done: false,
     important: false,
   };
-  onLabelDoneClick = () => this.setState(({ done }) => { return { done: !done } });
   onMarkImportant = () => this.setState(({ important }) => { return { important: !important } });
 
   render() {
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
 
     let classNames = ["todo-list-item"];
-    if (done) {
-      classNames.push("done");
-    }
-    if (important) {
-      classNames.push("important");
-    }
+    if (done) classNames.push("done");
+    if (important) classNames.push("important");
 
     return (
       <span className={classNames.join(" ")}>
@@ -26,10 +23,13 @@ export default class TodoListItem extends React.Component {
 
         <button
           type="button"
-          className={`btn ${this.state.done ? "btn-outline-success" : "btn-outline-danger"} btn-sm float-right"`}
-          onClick={this.onLabelDoneClick}
+          className={`btn ${
+            this.state.done ? "btn-outline-success" : "btn-outline-danger"
+          } btn-sm float-right"`}
+          onClick={onDeleted}
         >
-          {this.state.done ? <i className="fa-solid fa-plus" /> : <i className="fa-solid fa-times" />}
+        <Icon icon="mdi:minus" />
+        
         </button>
 
         <button
@@ -37,7 +37,11 @@ export default class TodoListItem extends React.Component {
           className="btn btn-outline-warning btn-sm float-right"
           onClick={this.onMarkImportant}
         >
-          {this.state.important ? <i className="fa-solid fa-arrow-down" /> : <i className="fa-solid fa-arrow-up" />}
+          {this.state.important ? (
+            <Icon icon="mdi:star-off" />
+          ) : (
+            <Icon icon="mdi:star" />
+          )}
         </button>
       </span>
     );
