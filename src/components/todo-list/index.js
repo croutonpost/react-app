508d591dfcf9e6a5be0202698044c3293ab9acbc
Chloe <chloe@sapphic.moe>
2024-10-19 12:39:19 +0500

    step 30: add ontoggle buttons

--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,18 +1,26 @@
 import React from "react";
-
 import TodoListItem from "../todo-list-item";
 import "./index.css";
 
 export default class TodoList extends React.Component {
   render() {
-    const { todos, onDeleted } = this.props;
+    const { 
+      todos, 
+      onDeleted, 
+      onToggleImportant, 
+      onToggleDone 
+    } = this.props;
+
     const elements = todos.map((item) => {
       const { id, ...itemProps } = item;
 
       return (
         <li key={id} className="list-group-item">
-          <TodoListItem {...itemProps}
+          <TodoListItem
+            {...itemProps}
             onDeleted={() => onDeleted(id)}
+            onToggleImportant={() => onToggleImportant(id)}
+            onToggleDone={() => onToggleDone(id)}
           />
         </li>
       );
